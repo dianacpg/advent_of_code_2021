9c8a709450260fcfbd78132d3fe43348b7b1d92a
dianacpg <dianacpgouveia@gmail.com>
2022-01-09 22:27:34 +0000

    refactor part1 and add part2

--- day3/day3.js
+++ day3/day3.js
@@ -1,41 +1,69 @@
 const fs = require("fs");
 
-//PART 1
-// Make array with the puzzle inputs
 const data = fs.readFileSync("./data.txt").toString().split("\n")
 
-//check how many bits each binary number has
-const binaryNumberLength = data[0].length
+//PART 1
+const countBits = ( array=data )  => {
+  const binaryNumberLength = array[0].length;
+  const binaryNumberArrays = new Array(binaryNumberLength).fill([])
+  
+  const allBitNumbers = binaryNumberArrays.map( (a,i) => {
+    return data.map(code => { return code.charAt(i)})
+  })
+ 
+  const zeroCounting = allBitNumbers.map(array => {
+    return array.filter( bit => bit == '0').length
+  })
 
-//Creates a new array with the collection of the bits numbers for each binary number position
-const binaryNumberArrays = new Array(binaryNumberLength).fill([])
+  const oneCounting = allBitNumbers.map(array => {
+    return array.filter( bit => bit == '1').length
+  })
+  return {zeroCounting, oneCounting};
+}
 
-const allBitNumbers = binaryNumberArrays.map( (array,i) => {
-   return data.map(code => { return code.charAt(i)})
-})
-
-
-//Counts how many 0 bits for each bit position by filtering the each array
-// and then checks its quantity on the bit array
-//Gamma has the most common bit for each position
-//Ellipson has the least common bit for each position
-const zeroCounting = allBitNumbers.map(array => {
-   return array.filter( bit => bit == '0').length
-})
-
-const gammabits = zeroCounting.map(quantity => {
+const gammabits = countBits(data).zeroCounting.map(quantity => {
     return quantity > (data.length/2) ? 0 : 1
 })
 
 const gamma = gammabits.join('')
 
-console.log("gamma",parseInt(gamma, 2))
 
-const ellipsonbits = zeroCounting.map(quantity => {
+const ellipsonbits = countBits(data).zeroCounting.map(quantity => {
     return quantity < (data.length/2) ? 0 : 1
 })
 
 const ellipson = ellipsonbits.join('')
 
 //power consumption = gamma rate * ellipson rate
-console.log("ellipson", parseInt(ellipson, 2) * parseInt(gamma, 2) )
+console.log("Power consumption", parseInt(ellipson, 2) * parseInt(gamma, 2))
+
+//PART2
+
+const filterMostCommon = (numbers, i=0) => {
+  const mostcommonbit= countBits(numbers).zeroCounting[i] > countBits(numbers).oneCounting[i] ? 0 : 1
+  const criteria = countBits(numbers).zeroCounting[i] == countBits(numbers).oneCounting[i] ? 1 : mostcommonbit;
+
+  const mostCommonNumbers = numbers.filter(number => number[i] == criteria)
+  if (mostCommonNumbers.length == 2) {
+    return mostCommonNumbers[1]
+  } else {
+    return filterMostCommon(mostCommonNumbers, i + 1)
+  }
+}
+
+const filterLeastCommon = (numbers, i=0) => {
+  const leastcommonbit= countBits(numbers).zeroCounting[i] < countBits(numbers).oneCounting[i] ? 0 : 1;
+  const criteria = countBits(numbers).zeroCounting[i] == countBits(numbers).oneCounting[i] ? 0 : leastcommonbit;
+
+  const leastCommonNumbers = numbers.filter(number => number[i] == criteria)
+  if (leastCommonNumbers.length == 2) {
+    return leastCommonNumbers[1]
+  } else {
+    return filterLeastCommon(leastCommonNumbers, i + 1)
+  }
+}
+console.log(filterMostCommon(data))
+console.log(filterLeastCommon(data))
+console.log(parseInt(filterMostCommon(data),2))
+console.log(parseInt(filterLeastCommon(data),2))
+console.log("support rating",parseInt(filterMostCommon(data),2)*parseInt(filterLeastCommon(data),2) )
